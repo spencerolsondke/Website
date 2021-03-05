a8f53288e03846ef65a878885fc951cf599d6ac5
Spencer Olson <34721984+spencerolsondke@users.noreply.github.com>
2021-03-05 23:11:07 +0100

    Initial website with sidebar

---
+++ components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+function ProfileComp(props){
+    return <div class="flex-col mb-5 mx-5">
+        <div class="flexmb-1 mb-2 rounded-full ring-4 ring-white-dark overflow-hidden shadow-md">
+            <img class="flex-grow"
+                src="/images/profile-small.jpg"
+                alt="Spencer Matei Olson"
+                />
+        </div>
+        <p class="mb-10 text-white text-center text-lg">Spencer Matei Olson</p>
+    </div>
+}
+
+function Button(props) {
+    return <div class="flex mb-3">
+            <Link href={props.href}>
+                <a class="border rounded-md hover:bg-blue-dark flex-grow text-center text-white">{props.text}</a>
+            </Link>
+        </div>
+}
+
+export const Sidebar = () => {
+    return (
+        <>
+            <div class="flex min-w-1/8 shadow-lg px-5 h-screen w-1/8 bg-gray-700">
+                <div class="flex-col my-10 mx-auto align-items">
+                    <ProfileComp width = "160" height="150"/>
+                    <Button text="Home" href="/posts/first-post" />
+                    <Button text="Posts" href="/posts/landing" />
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: ['./pages/**/*.{js}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          dark: '#2e3440',
+          DEFAULT: '#3b4252',
+          light: '#4c566a',
+        },
+        white: {
+          DEFAULT: '#eceff4',
+          dark: '#d8dee9'
+        },
+        blue: {
+          DEFAULT: '#81a1c1',
+          dark: '#5e81ac',
+          light: '#88c0d0',
+        }
+      },
+      minWidth: {
+        '10': '10rem',
+      },
+      spacing: {
+        '1/8': '12.5%'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
