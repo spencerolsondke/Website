c2051facd635665d429a871ce88c65100ea9591a
Spencer Olson <34721984+spencerolsondke@users.noreply.github.com>
2021-03-06 21:17:22 +0100

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,11 @@
 
 
 export default function Card({children}) {
-    <div class="h-screen">
-        <div class="my-auto">
-            {children}
+    return (
+        <div class="h-screen">
+            <div class="my-auto">
+                {children}
+            </div>
         </div>
-    </div>
+    )
 }
